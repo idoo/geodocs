694c6072eda7a5fc246344bf349e39cc5dd8ab3c
Ivan Verevkin <vereva@x-root.org>
2013-08-30 10:59:50 +0400

    welcome again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if current_user.sign_in_count == 1
       edit_user_registration_path
-    elsif Item.where('created_at > ?', current_user.last_sign_in_at).count > 0
-      news_path
+    #elsif Item.where('created_at > ?', current_user.last_sign_in_at).count > 0
+    #  news_path
     else
       root_path
     end
